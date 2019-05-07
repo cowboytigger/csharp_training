62430d4c1437829b83446be358f29077462059e4
cowboytigger <tigger.cowboy@gmail.com>
2019-05-07 12:45:24 +0300

    Задание 6

---
+++ adressbook-web-tests/adressbook-web-tests/Appmanager/GroupHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    public class GroupHelper : HelperBase
+    {
+        public GroupHelper(IWebDriver driver) 
+            : base(driver)
+        {
+        }
+
+        public void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        public void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        public void FillGroupForm(GroupData group)
+        {
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        public void SelectGroup(int index)
+        {
+            driver.FindElement(By.XPath("(//input[@name='selected[]'])[3]")).Click();
+        }
+
+        public void RemoveGroup()
+        {
+            driver.FindElement(By.XPath("(//input[@name='delete'])[2]")).Click();
+        }
+
+        public void ReturnToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+    }
+}
---
+++ adressbook-web-tests/adressbook-web-tests/Tests/ContactCreationTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactsCreationTest : TestBase
+    {
+        [Test]
+        public void ContactCreationTest()
+        {
+            app.Navigator.GoToHomePage();
+            app.Auth.Login(new AccountData("admin", "secret"));
+            app.Contacts.InitContactCreation();
+            app.Contacts.FillContactForm(new ContactData("amanda", "rose"));
+            app.Contacts.SubmitContactCreation();
+            app.Auth.Logout();
+        }
+
+    }
+}
